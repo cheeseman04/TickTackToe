74872e451cffcfceb1da11d3a7be232594d7e5ef
Tom Purvis <tom.purvis@live.com>
2023-03-14 21:24:38 -0400

    Rework DOM method to show scoreboard on initial UI Initally, scoreboard showed only after the initial buttons were pushed by user. Add scoreboard that is viewable on initial page load to show a "0-0" score - which then updates as user makes selections.
    Add HTML divs with classes, target those classes with querySelec,
    use button function to pass iterating score to the
    "const" set at the top with noted querySelec.
    
    Update css to center scoreboard.

--- rps.js
+++ rps.js
@@ -1,107 +1,100 @@
-const container = document.querySelector('#container');
-        const results = document.createElement ('div');
-        results.classList.add('results');
+let computerScore = 0;
+let humanScore = 0;
+let draws = 0;
+// This function randomizes the computer's choice
+const answer = ["rock", "paper", "scissors"];
 
-        const computer = document.createElement ('div')
-        computer.classList.add('score')
-
-        const human = document.createElement ('div')
-        human.classList.add('score')
+const scoreHuman = document.querySelector('.human-score')
+const scoreComputer = document.querySelector('.computer-score')
+const scoreUpdate = document.querySelector('.score-update')
 
 
 
-        let computerScore = 0;
-        let humanScore = 0;
-        let draws = 0;
-        // This function randomizes the computer's choice
-        const answer = ["rock", "paper", "scissors"];
 
         
-        function getComputerChoice () {
-            const random = Math.floor(Math.random()*answer.length);
-            return answer[random];
+function getComputerChoice () {
+    const random = Math.floor(Math.random()*answer.length);
+    return answer[random];
 
-        }
+}
 
-        //does all of this need to go inside the loop?
-        
-        const buttons = document.querySelectorAll('button');
-        buttons.forEach((button) => {
-            button.addEventListener('click', () => {
-                const playerSelection = button.id;
-                const computerSelection = getComputerChoice ();
-                let result = playRound (playerSelection, computerSelection);
-                console.log (result);
-                gameScore (result);
-                console.log ("Computer Score:" + computerScore)
-                console.log ("Human Score:" + humanScore)
+//does all of this need to go inside the loop?
 
-                results.textContent = result
-                container.appendChild(results);
+const buttons = document.querySelectorAll('button');
+buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+        const playerSelection = button.id;
+        const computerSelection = getComputerChoice ();
+        let result = playRound (playerSelection, computerSelection);
+        gameScore (result);
+        console.log (playRound (playerSelection, computerSelection))
 
-                computer.textContent = "Computer Score:" + computerScore
-                human.textContent = "Human Score:" + humanScore
-                container.appendChild(computer);
-                container.appendChild(human);
+        scoreHuman.textContent = humanScore;
+        scoreComputer.textContent = computerScore;
+        scoreUpdate.textContent = result;
 
-                
-                
-            });
-        });
+
+
+
 
         
-
-        //This function sets up who wins and loses between human and computer based on their choices
-        function playRound (playerSelection, computerSelection) {
-            if (playerSelection === computerSelection) {
-                return ("tie");
-            } else if (playerSelection === "paper" && computerSelection === "rock") {
-                return ("humanWins");
-            } else if (playerSelection === "rock" && computerSelection === "scissors") {
-                return ("humanWins");
-            } else if (playerSelection === "scissors" && computerSelection === "paper") {
-                return ("humanWins")
-            } else {
-                return ("computerWins")
-            }
-        }
-
         
-        // loops a game of RPS, calls score function to display score after each round
-        // function game () {
-        //     for (i=0; i<10; i++) {
-        //         let playerSelection = prompt ("please make your selection:").toLowerCase ();
-        //         let computerSelection = getComputerChoice ();
-        //         let result = playRound (playerSelection, computerSelection);
-        //         console.log (result);
-        //         gameScore (result);
-        //         console.log ("Computer Score:" + computerScore)
-        //         console.log ("Human Score:" + humanScore)
+    });
+});
 
-        //         if (humanScore === 3 || computerScore === 3) {
-        //             break;
-        //         }
-        //     }
-        // }
-        //determines score based on results of the game
-        function gameScore (result) {
-            if (result === "humanWins") {
-                humanScore++;
-            } else if (result === "tie") {
-                draws++
-            } else {
-                computerScore++
-            }
 
-            if (humanScore === 5) {
-                console.log ("Game Over. Human Wins"); 
-                alert ("Game Over. Human Wins!")  
-            } 
 
-            if (computerScore === 5) {
-                console.log ("Game Over. Computer Wins");
-                alert ("Game Over. Computer Wins.")
-            }
-        }
+//This function sets up who wins and loses between human and computer based on their choices
+function playRound (playerSelection, computerSelection) {
+    if (playerSelection === computerSelection) {
+        return ("tie");
+    } else if (playerSelection === "paper" && computerSelection === "rock") {
+        return ("humanWins");
+    } else if (playerSelection === "rock" && computerSelection === "scissors") {
+        return ("humanWins");
+    } else if (playerSelection === "scissors" && computerSelection === "paper") {
+        return ("humanWins")
+    } else {
+        return ("computerWins")
+    }
+}
 
-        // console.log (game())
+
+// loops a game of RPS, calls score function to display score after each round
+// function game () {
+//     for (i=0; i<10; i++) {
+//         let playerSelection = prompt ("please make your selection:").toLowerCase ();
+//         let computerSelection = getComputerChoice ();
+//         let result = playRound (playerSelection, computerSelection);
+//         console.log (result);
+//         gameScore (result);
+//         console.log ("Computer Score:" + computerScore)
+//         console.log ("Human Score:" + humanScore)
+
+//         if (humanScore === 3 || computerScore === 3) {
+//             break;
+//         }
+//     }
+// }
+//determines score based on results of the game
+function gameScore (result) {
+    if (result === "humanWins") {
+        humanScore++;
+    } else if (result === "tie") {
+        draws++
+    } else {
+        computerScore++
+    }
+
+    if (humanScore === 5) {
+        console.log ("Game Over. Human Wins"); 
+        alert ("Game Over. Human Wins!")  
+    } 
+
+    if (computerScore === 5) {
+        console.log ("Game Over. Computer Wins");
+        alert ("Game Over. Computer Wins.")
+    }
+}
+
+// console.log (game())
